b187d6befbda01cf452d454aefb0d54c5aef4b1c
Aryan Patel <arynptl1412@gmail.com>
2026-01-27 12:38:02 +0530

    Backend Development First Commit on Day-2 of learning

---
+++ Day-2/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express(); //server instance created
+
+app.get("/", (req, res)=>{
+    res.send("Server is running.")
+})
+
+app.get("/about", (req, res)=>{
+    res.send("This is about page bhenchod")
+})
+
+app.get("/home", (req, res)=>{
+    res.send("This is home page bhenchod")
+})
+
+app.listen(3000, ()=>{ // server start karna
+    console.log("Server is online.")
+})
