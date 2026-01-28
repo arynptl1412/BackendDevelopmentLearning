3b92adcd709f4a62dffb372007746628646da4c6
Aryan Patel <arynptl1412@gmail.com>
2026-01-29 00:19:52 +0530

    Day-3 Postman started

--- Day-3/server.js
+++ Day-3/server.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send("Server is running")
+const notes = []
+
+app.use(express.json())
+
+app.post("/notes", (req, res)=>{
+    
+    notes.push(req.body)    
+
+    res.send("notes created")
+})
+
+app.get("/notes", (req, res)=>{
+    res.send(notes)
 })
 
 app.listen(3000, ()=>{
