69924fdd34abf877a4032c99944ff1e60c0cf520
Aryan Patel <arynptl1412@gmail.com>
2026-02-07 22:57:16 +0530

    Day-9 Started Deployment

--- Day-8/Backend/src/app.js
+++ Day-8/Backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const noteModel = require("./models/note.model");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('./public'))
 
 app.post("/api/notes", async (req, res)=>{
     const {title, desc} = req.body;
@@ -49,6 +51,10 @@ app.patch("/api/notes/:id", async (req, res)=>{
     res.status(200).json({
         message:"Note updated Successdully."
     })
+})
+
+app.use('*name', (req, res)=>{
+    res.sendFile(path.join(__dirname,".." ,"/public/index.html"))
 })
 
 module.exports = app;
--- Day-8/Frontend/src/App.jsx
+++ Day-8/Frontend/src/App.jsx
@@ -1,87 +1,79 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 const App = () => {
 
-  const [notes, setnotes] = useState([
-    {
-      title: "Test Title 1",
-      desc: "Test Description 1"
-    },
-    {
-      title: "Test Title 2",
-      desc: "Test Description 2"
-    },
-    {
-      title: "Test Title 3",
-      desc: "Test Description 3"
-    },
-    {
-      title: "Test Title 4",
-      desc: "Test Description 4"
-    },
-    {
-      title: "Test Title 1",
-      desc: "Test Description 1"
-    },
-    {
-      title: "Test Title 2",
-      desc: "Test Description 2"
-    },
-    {
-      title: "Test Title 3",
-      desc: "Test Description 3"
-    },
-    {
-      title: "Test Title 4",
-      desc: "Test Description 4"
-    },
-    {
-      title: "Test Title 1",
-      desc: "Test Description 1"
-    },
-    {
-      title: "Test Title 2",
-      desc: "Test Description 2"
-    },
-    {
-      title: "Test Title 3",
-      desc: "Test Description 3"
-    },
-    {
-      title: "Test Title 4",
-      desc: "Test Description 4"
-    },
-    {
-      title: "Test Title 1",
-      desc: "Test Description 1"
-    },
-    {
-      title: "Test Title 2",
-      desc: "Test Description 2"
-    },
-    {
-      title: "Test Title 3",
-      desc: "Test Description 3"
-    }
-  ])
+  const [notes, setnotes] = useState([]);
 
-  axios.get('http://localhost:3000/api/notes')
-    .then((res) => {
-      setnotes(res.data.notes)
+  async function fetchNotes(){
+    try {
+      const res = await axios.get("http://localhost:3000/api/notes");
+      setnotes(res.data.notes);
+    } catch (e) {
+      console.log("API was not successfully fetched.");
+    }
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, [])
+
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    const {title, description} = e.target.elements;
+
+    await axios.post("http://localhost:3000/api/notes",{
+      title: title.value,
+      desc: description.value
     })
-    .catch((e) => {
-      console.log("Api was not fetched Successfully", e);
-    })
+
+    e.target.reset();
+    fetchNotes();
+  }
+
+  async function handleDeleteNode(noteId){
+    try{
+      const res = await axios.delete(`http://localhost:3000/api/notes/${noteId}`);
+      fetchNotes();
+      
+    }catch(e){
+      console.log(e);
+    }
+  }
 
   return (
-    <div className='notes h-screen w-screen p-6 bg-neutral-900 flex flex-wrap gap-10 justify-center overflow-y-auto'>
-      {notes.map((note, index) => {
-        return <div key={index} className="note bg-neutral-700 h-60 w-60 border-white border-2 rounded text-white p-2 flex flex-col items-center gap-3">
-          <h1>{note.title}</h1>
-          <p>{note.desc}</p>
+    <div className='notes h-screen w-screen p-6 bg-neutral-900 flex flex-col gap-10 items-center overflow-y-auto'>
+
+      <form className='text-white bg-neutral-500 p-10 rounded-3xl flex flex-col gap-6' onSubmit={handleSubmit}>
+        <h1 className='text-3xl font-bold'>Enter the Details of the Notes</h1>
+
+        <div className="details">
+          <div className="titleInput flex flex-col gap-2">
+            <label htmlFor="title">Enter Label:</label>
+            <input name='title' required type="text" className='bg-white px-2 p-1 rounded-xl text-black' />
+          </div>
+
+          <div className="descInput flex flex-col gap-2">
+            <label htmlFor="title">Enter Description:</label>
+            <textarea name='description' required type="" className='bg-white px-2 p-1 rounded-xl text-black h-50' />
+          </div>
+
         </div>
-      })}
+        <button className='bg-emerald-500 w-fit m-auto px-3 py-1 rounded-3xl'>Create Note</button>
+      </form>
+
+      <div className="allNotes flex gap-10 justify-center flex-wrap">
+        {notes.map((note, index) => {
+          return <div key={index} className="note bg-neutral-700 border-white border-2 rounded text-white px-10 py-3.5 flex flex-col items-center gap-3 max-w-100">
+            <h1 className='font-bold text-3xl'>{note.title}</h1>
+            <p>{note.desc}</p>
+            <button className='bg-red-500 px-2 py-1 rounded' onClick={()=>{
+              handleDeleteNode(note._id)
+            }}>Delete</button>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
